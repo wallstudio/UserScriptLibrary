ddba8ce36dffafd3519d06f74ce423fd787d7717
UpHash <yukawallstudio@gmail.com>
2020-09-29 10:19:58 +0900

    Error throw

--- xhrFetchInjection.js
+++ xhrFetchInjection.js
@@ -3,7 +3,14 @@ injectXHR = function(action)
     let base_open = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function(method, url, async, user, password)
     {
-        arguments = action(url, arguments);
+        try
+        {
+            arguments = action(url, arguments);
+        }
+        catch(e)
+        {
+            console.error(e);
+        }
         base_open.apply(this, arguments);
     };
     console.log("Injected XHR " + action);
@@ -14,8 +21,15 @@ injectFetch = function(action)
     let base_fetch = fetch;
     fetch = function(input, init)
     {
-        let url = input ? input.url : "";
-        arguments = action(url, arguments);
+        try
+        {
+            let url = input ? input.url : "";
+            arguments = action(url, arguments);
+        }
+        catch(e)
+        {
+            console.error(e);
+        }
         return base_fetch.apply(null, arguments);
     };
     console.log("Injected Fetch " + action);
